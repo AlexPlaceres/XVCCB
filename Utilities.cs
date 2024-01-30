164d6d57aa5deabbf571b19174766fef37fb6d31
AlexPlaceres <alexis.placeres@upr.edu>
2024-01-30 13:35:09 -0400

    Implemented ReadNullTerminatedString Method

--- Utilities.cs
+++ Utilities.cs
@@ -12,18 +12,39 @@ public static class ParseExtensions
 
         if (alignmentPaddingSize > 0 && alignmentPaddingSize < BlockSize)
         {
-            Console.WriteLine("\t\t  Aligning...");
-            Console.WriteLine("\t\t  Current Offset = {0}", Offset);
-            Console.WriteLine("\t\t  Seeking forward by {0} bytes to align to nearest {1} byte block", alignmentPaddingSize, BlockSize);
+            //Console.WriteLine("\t\t  Aligning...");
+            //Console.WriteLine("\t\t  Current Offset = {0}", Offset);
+            //Console.WriteLine("\t\t  Seeking forward by {0} bytes to align to nearest {1} byte block", alignmentPaddingSize, BlockSize);
 
             reader.BaseStream.Seek(alignmentPaddingSize, SeekOrigin.Current);
-            Console.WriteLine("\t\t  New Position: {0}", reader.BaseStream.Position);
+            //Console.WriteLine("\t\t  New Position: {0}", reader.BaseStream.Position);
         }
         else
         {
-            Console.WriteLine("\t\t  No Alignment Needed");
-            Console.WriteLine("\t\t  Current Offset = {0}", Offset);
+            //Console.WriteLine("\t\t  No Alignment Needed");
+            //Console.WriteLine("\t\t  Current Offset = {0}", Offset);
         }
+    }
+
+    
+    public static string ReadNullTerminatedString(this BinaryReader reader)
+    {
+        int numChars = 0;
+        long returnPoint = reader.BaseStream.Position;
+
+        // Increments numChars until null termination is reached
+        while (reader.ReadChar() != 0x0)
+        {
+            numChars += 1;
+        }
+
+        // Returns to string's starting point and reads characters into char array
+        reader.BaseStream.Position = returnPoint;
+        char[] charStr = reader.ReadChars(numChars+1);
+        
+
+        return new string(charStr);
+
     }
 }
 

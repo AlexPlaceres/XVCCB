1878884a914b46dc7986bc6cfa13661661e2a5b9
AlexPlaceres <alexis.placeres@upr.edu>
2024-01-31 12:04:39 -0400

    New SEDB Reading / Writing Methods
    - Changed Name to byte array to read beyond 0x3F
    
    - Added ReadName method
    
    - Added Write method

--- SectionDataBinary.cs
+++ SectionDataBinary.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Text;
 using XVCCB.Serialization;
+using XVCCB.Utilities;
 
 namespace XVCCB.Data;
 
@@ -15,7 +16,8 @@ public class SectionDataBinary
     public ushort Offset { get; set; }
     public ulong Size { get; set; }
     public ulong DateTime {  get; set; }
-    public char[] Name { get; set; }
+    public byte[] Name { get; set; } = new byte[16];
+    public String StrName {  get; set; }
 
     public void Read(BinaryReader reader)
     {
@@ -34,6 +36,45 @@ public class SectionDataBinary
         DateTime = reader.ReadUInt64();
         Console.WriteLine("\tFile Size = {0} Bytes, Offset = {1} Bytes\n", Size, Offset);
 
-        Name = reader.ReadChars(16);
+        reader.Read(Name);
+
+
+
+        if (new String(SubType) == "mtb\x0")
+        {
+            ReadName(reader);
+        }
+    }
+
+    private void ReadName(BinaryReader reader)
+    {
+        unsafe
+        {
+            fixed (byte* pName = &Name[0])
+            {
+                uint nameOffset = *(uint*)pName & 0x00FFFFFF;
+
+                long returnPoint = reader.BaseStream.Position;
+                reader.BaseStream.Position = (returnPoint - 16) + (long)nameOffset;
+
+                StrName = reader.ReadNullTerminatedString();
+
+                reader.BaseStream.Position = returnPoint;
+            }
+
+        }
+    }
+
+    public void Write(BinaryWriter writer)
+    {
+        writer.Write(Type);
+        writer.Write(SubType);
+        writer.Write(Version);
+        writer.Write((byte)EndianType);
+        writer.Write(AlignmentBits);
+        writer.Write(Offset);
+        writer.Write(Size);
+        writer.Write(DateTime);
+        writer.Write(Name);
     }
 }
